2d34183b99148226cd71733628edd4fd3e5f8296
Maka I <nibe0993@gmail.com>
2021-04-23 09:34:36 +0100

    Add requirements text

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 import setuptools
+import os
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(DIR, "docs", "README.md"), "r") as f:
+    long_description = f.read()
+with open(os.path.join(DIR, "requirements.txt"), "r") as f:
+    REQUIRED = [i for i in f.read().split("\n") if len(i)]
 
 
 setuptools.setup(
@@ -14,7 +22,8 @@ setuptools.setup(
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/the-code-bae/mi-property-analyser",
-     packages=setuptools.find_packages(),
+     packages=setuptools.find_packages(include=['mi-property-analyser']),
+     install_requires=REQUIRED
      python_requires=">=3.5",
      classifiers=[
          "Programming Language :: Python :: 3",
