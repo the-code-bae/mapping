89f52681afc92850e2c88d8ed0c4c01739ee6f7c
Maka I <nibe0993@gmail.com>
2021-04-27 22:17:47 +0100

    Remove comments

--- mi_property_analyser/london_stations.py
+++ mi_property_analyser/london_stations.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import os
 
+
 def load_london_stations():
-	# need to enter a way of getting file path
-	filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/london_stations.csv')
+    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/london_stations.csv')
 
-	return pd.read_csv(filepath)
-
+    return pd.read_csv(filepath)
