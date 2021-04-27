7840fcf6f5e2fe55523b341084e722f643a5daa6
Maka I <nibe0993@gmail.com>
2021-04-27 22:19:58 +0100

    Add documentation about where file is downloaded from

--- mi_property_analyser/london_stations.py
+++ mi_property_analyser/london_stations.py
@@ -3,6 +3,9 @@ import os
 
 
 def load_london_stations():
-    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/london_stations.csv')
+	"""This CSV file has been downloaded from this link
+	https://www.doogal.co.uk/london_stations.php """
 
-    return pd.read_csv(filepath)
+	filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/london_stations.csv')
+
+	return pd.read_csv(filepath)
